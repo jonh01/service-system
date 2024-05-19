4d2cd5172f805e1ee34f16fe1476b01928935450
Jhony de Paula <74214020+jonh01@users.noreply.github.com>
2024-05-19 12:42:56 -0300

    att project
    fix NonNull problem

--- api/src/main/java/com/servicesystem/api/domain/repositories/CategoryRepository.java
+++ api/src/main/java/com/servicesystem/api/domain/repositories/CategoryRepository.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import com.servicesystem.api.domain.models.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
-    Page<Category> findAll(Pageable pageable);
+    @NonNull
+    Page<Category> findAll(@NonNull Pageable pageable);
 
     boolean existsByName(String name);
 
